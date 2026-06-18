ed9c3f881a545acac170af043a58cc69582e0e0d
orallarooo <kdirbaevoral875@gmail.com>
2026-06-18 16:38:22 +0500

    --add svgLoader

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,18 @@
-import { Suspense } from "react";
 import './styles/index.scss'
-import { Routes, Route, Link } from 'react-router-dom'
 import { useTheme } from "app/providers/ThemeProvider";
 import { classNames } from "../shared/lib/classNames/classNames";
-import AboutPage from "pages/AboutPage/ui/AboutPage";
-import MainPage from "pages/MainPage/ui/MainPage";
 import { AppRouter } from "./providers/router";
+import { Navbar } from 'widgets/Navbar';
 
 
 
 const App = () => {
     
-    const {theme, toggleTheme} = useTheme()
+    const {theme} = useTheme()
 
     return ( 
         <div className={classNames('app',{},[theme])}>
-            <button onClick={toggleTheme}>Toggle</button>
-            <Link to={'/'}>Glavnaya</Link>
-            <Link to={'/about'}>Aboutttt</Link>
+            <Navbar />
             <AppRouter />
         </div>
     )
